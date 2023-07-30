5620ba7f84a99f111af879412bca284ffd75ddf8
Nastyanastya102 <little.smart.bee@gmail.com>
2023-07-30 17:15:22 +0100

    Added refresh plugings

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -6,6 +6,7 @@ export function buildDevServer (options: BuildOptions): Configuration {
   return {
     port: options.port,
     open: true,
-    historyApiFallback: true
+    historyApiFallback: true,
+    hot: true
   }
 }
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,9 +3,10 @@ import webpack, { DefinePlugin } from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import { BuildOptions } from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin"
 
 export function buildPlugins ({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
+  const plugins = [
     new HtmlWebpackPlugin({
       template: paths.html,
     }),
@@ -17,6 +18,13 @@ export function buildPlugins ({paths, isDev}: BuildOptions): webpack.WebpackPlug
     }),
     new DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev)
-    })
+    }),
   ]
+
+  if (isDev) {
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+    plugins.push(new ReactRefreshWebpackPlugin());
+  };
+
+  return plugins;
 }
