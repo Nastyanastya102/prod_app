bbfa35b407085fc19805c8810c726cc05868c7b0
Nastyanastya102 <little.smart.bee@gmail.com>
2023-08-02 23:37:26 +0100

    Fix test for side bar

---
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -0,0 +1,22 @@
+import { render } from '@testing-library/react';
+import React, { ReactNode } from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { I18nextProvider } from 'react-i18next';
+
+import i18nTesting from 'shared/config/i18n/i18nTesting';
+
+export interface IRenderComponent {
+  route?: string;
+}
+export const ComponentRender = (component: ReactNode, options: IRenderComponent = {}) => {
+  const {
+    route = "/"
+  } = options;
+  return render(
+    <MemoryRouter initialEntries={[route]}>
+      <I18nextProvider i18n={i18nTesting}>
+        {component}
+      </I18nextProvider>
+    </MemoryRouter>
+  )
+};
--- src/widgets/SideBar/ui/SideBar/SideBar.test.tsx
+++ src/widgets/SideBar/ui/SideBar/SideBar.test.tsx
@@ -1,19 +1,19 @@
 import { fireEvent, screen } from '@testing-library/react';
-import React from 'react';
 
 import { SideBar } from 'widgets/SideBar/ui/SideBar/SideBar';
 import {
     RenderWithTranslation,
 } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { ComponentRender } from 'shared/lib/tests/componentRender/componentRender';
 
 describe('SideBar', () => {
     test('SideBar was rendered', () => {
-        RenderWithTranslation(<SideBar />);
+        ComponentRender(<SideBar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
 
     test('SideBar collapsed', () => {
-        RenderWithTranslation(<SideBar />);
+        ComponentRender(<SideBar />);
         const btn = screen.getByTestId('sidebar-toggle');
         fireEvent.click(btn);
         expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
