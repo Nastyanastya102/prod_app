92d9f2bf6d603c9b28ebd956fe53fbe4946665b9
Nastyanastya102 <little.smart.bee@gmail.com>
2023-11-16 14:21:43 -0800

    fix stories for login form

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,9 +1,14 @@
 import { Story } from '@storybook/react';
 import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
-import { DeepPartial } from '@reduxjs/toolkit';
+import { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
+import { loginReducer } from 'features/AuthByUsername/model/slice/loginSlice';
+
+const defaultReducers: DeepPartial<ReducersMapObject<StateSchema>> = {
+    loginForm: loginReducer,
+};
 
 export const StoreDecorator = (state: DeepPartial<StateSchema>) => (StoryComponent: Story) => (
-    <StoreProvider initialState={state}>
+    <StoreProvider initialState={state} asyncReducers={defaultReducers}>
         <StoryComponent />
     </StoreProvider>
 );
