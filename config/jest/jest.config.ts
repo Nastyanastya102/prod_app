d7899fcee2481dfe4dc97100714805f347eb4645
StacyKlyashtorna <stacy.klyashtorna@tempi.com>
2024-06-10 19:35:54 -0700

    fix types error & test fix

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -8,7 +8,7 @@ import path from 'path';
 export default {
     globals: {
         __IS_DEV__: true,
-        __API: '',
+        __API__: '',
     },
     clearMocks: true,
     testEnvironment: 'jsdom',
