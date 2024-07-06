95843ac756fbff1482edfbc43df6f9986ac74eec
Nastyanastya102 <little.smart.bee@gmail.com>
2024-07-05 21:39:53 -0700

    Change port

--- webpack.config.ts
+++ webpack.config.ts
@@ -15,7 +15,7 @@ export default (env: BuildEnv) => {
     const PORT = env.port || 3000;
 
     const isDev = mode === 'development';
-    const apiUrl = env.apiUrl || 'http://localhost:8080';
+    const apiUrl = env.apiUrl || 'http://localhost:8000';
 
     const config: webpack.Configuration = buildWebpackConfig({
         mode,
