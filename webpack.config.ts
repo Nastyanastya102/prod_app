e5a221cae2aad6011f3de2670bdc67bd3ae549f2
Nastyanastya102 <little.smart.bee@gmail.com>
2024-08-14 22:21:29 -0700

    fix API route

--- webpack.config.ts
+++ webpack.config.ts
@@ -17,7 +17,7 @@ export default (env: BuildEnv) => {
     const PORT = env.port || 3000;
 
     const isDev = mode === 'development';
-    const apiUrl = env.apiUrl || 'http://localhost:8000';
+    const apiUrl = env.apiUrl ?? 'http://localhost:8000';
 
     const config: webpack.Configuration = buildWebpackConfig({
         mode,
